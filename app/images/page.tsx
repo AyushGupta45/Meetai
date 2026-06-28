7ac800e951119165136a07478830e881fcc8c2ad
AyushGupta45 <guptaayushrajeshwar@gmail.com>
2026-06-28 13:13:31 +0530

    feat: add images for dos and donts along with page component

---
+++ app/images/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <img src="/dos.png" alt="page" />
+      <img src="/donts.png" alt="page" />
+    </>
+  );
+};
+
+export default page;
